fec8e04d1c9b774539a0ed1700f5acf03276eff6
Neil Castelino <neil.fl93@gmail.com>
2017-11-21 17:54:01 -0500

    Added initial server

---
+++ server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
+
+func OpenListener(port int, new_connection chan net.Conn, what chan bool) {
+	ln, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	if err != nil {
+		fmt.Println("OMG 1: Neil do something!")
+		fmt.Println(err)
+	}
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				fmt.Println("OMG 2: Neil do something!")
+			}
+			new_connection <- conn
+		}
+		what <- true
+	}()
+
+}
+
+func GetPortFromPtr(port *int) int {
+	x := fmt.Sprintf("%d", *port)
+	p, _ := strconv.Atoi(x)
+	return p
+}
+
+func main() {
+	miner_port_flag := flag.Int("mport", 9876,
+		"Port on which server listens on for miner")
+	client_port_flag := flag.Int("cport", 9875,
+		"Port on which server listens on client")
+
+	flag.Parse()
+
+	var miner_port int = GetPortFromPtr(miner_port_flag)
+	var client_port int = GetPortFromPtr(client_port_flag)
+
+	new_miner_connection := make(chan net.Conn)
+	new_client_connection := make(chan net.Conn)
+
+	// TODO: Get rid of this channel eventually
+	what := make(chan bool)
+
+	OpenListener(miner_port, new_miner_connection, what)
+	OpenListener(client_port, new_client_connection, what)
+
+	// TODO: remove stuff beyond this point
+	<-what
+	<-what
+	fmt.Println("Debug: Success")
+	os.Exit(0)
+}
